52f23851b811773720b6d67a3f625fd486062ab4
DevRahat <dev.rahatislam@gmail.com>
2024-03-13 02:46:28 +0600

    commit

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -5,18 +5,55 @@ import './Countries.css';
 
 const Countries = () => {
     const [countries, setCountries]=useState([]);
+    const [visitedCountries, setVisitedCountries]=useState([]);
+    const [visitedFlags, setVisitedFlag]=useState([]);
 
     useEffect(()=>{
         fetch('https://restcountries.com/v3.1/all')
         .then(res =>res.json())
         .then(data => setCountries(data));
     },[])
+
+    
+    const handleVisitedCountry=country=>{
+        const newVisitedCountries=[...visitedCountries,country];
+        setVisitedCountries(newVisitedCountries);
+    }
+
+    const handleVisitedFlags= flag =>{
+        const newVisitedFlag=[...visitedFlags, flag];
+        setVisitedFlag(newVisitedFlag);
+    }
+    // remove item from an array in state
+    //  use filter to select all the elements except the one you want to remove
+
+
+
     return (
         <div>
             <h3>Countries: {countries.length}</h3>
+
+            <div className="flag-container">
+                {
+                    visitedFlags.map((flag, idx)=> <img key={idx} src={flag}></img>)
+                }
+            </div>
+
+            {/* vivited country */}
+            <div>
+                <h5>Visited countries</h5>
+                <ul>
+
+                </ul>
+            </div>
+
+            {/* Display */}
             <div className="country-container">
             {
-                countries.map(country =><Country key={country.cca3} country={country}></Country>)
+                countries.map(country =><Country key={country.cca3} 
+                handleVisitedCountry={handleVisitedCountry}
+                handleVisitedFlags={handleVisitedFlags} 
+                country={country}></Country>)
             }
             </div>
         </div>
--- src/components/Countries/Country.jsx
+++ src/components/Countries/Country.jsx
@@ -1,21 +1,29 @@
 import { useState } from 'react';
 import '../Country/Country.css'
-const Country = ({country}) => {
+const Country = ({country, handleVisitedCountry, handleVisitedFlags}) => {
     const {name, flags, population, area, cca3}=country;
 
     const [visited, setVisited]=useState(false);
 
     const handleVisited= ()=>{
-        setVisited(true);
+        setVisited(!visited);
     }
+
+    const passWithParams=()=>handleVisitedCountry(country);
+    
     return (
-        <div className='country'>
-            <h3>Name: {name?.common}</h3>
+        <div className={`country ${visited && 'visited'}`}>
+            <h3 style={{color: visited ? 'purple': 'white'}}>Name: {name?.common}</h3>
             <img src={flags.png} alt="" />
             <p>Population: {population}</p>
             <p>area: {area}</p>
             <p><small>Code: {cca3}</small></p>
-            <button onClick={handleVisited}>Visited</button>
+            <br />
+            <button onClick={()=>handleVisitedFlags(country.flags.png)}>Add Flag</button>
+            
+            <button onClick={()=>handleVisitedCountry}>Mark As visited</button>
+            <br />
+            <button onClick={handleVisited}>{visited ? 'Visited':'Going'} </button>
             {visited ? 'I have Visited this country': 'I Want to visit'}
             
 
